a979d1fee81929f95fa96881be0481aeed9995a5
Tiamiyukorede <akoredegold44@gmail.com>
2022-12-14 00:12:49 -0800

    finds and prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -42,7 +42,9 @@ int main(void)
 			n2 = fn;
 			n22 = (fn2 % 1000000000);
 		}
-		printf("\n");
-		return (0);
+		if (((n1 + n2) < 0) && boolean == 1)
+			boolean = 0;
 	}
+	printf("\n");
+	return (0);
 }
