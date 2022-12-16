6d338b7192c478f234d572965b45901bc56af42f
Tiamiyukorede <akoredegold44@gmail.com>
2022-12-15 18:16:40 -0800

    Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - multiply 2 num
  * @a: first num
  * @b: second num
- * Return the addition of the multiplication
+ * Return: the addition of the multiplication
  */
 
 int mul(int a, int b)
