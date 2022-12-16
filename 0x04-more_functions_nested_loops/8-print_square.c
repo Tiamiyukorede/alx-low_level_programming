98bcecf5125f4bfdfe4a430eff73ce8899434cdc
Tiamiyukorede <akoredegold44@gmail.com>
2022-12-16 04:58:14 -0800

    prints a square, followed by a new line

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_square - prints hashes squares
  * @size: size of the square
@@ -11,7 +12,7 @@ void print_square(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size; j++)
+		for (j = 0; j < size; j++)
 		{
 			_putchar(35);
 		}
