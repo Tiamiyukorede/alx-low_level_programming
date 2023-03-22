4cda37bedba56951edb38974e614429bb798fb73
Tiamiyukorede <akoredegold44@gmail.com>
2023-03-22 04:43:48 -0700

    task 0

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code for a digit 0 through 9
+ * @c: char to check
+ * Return: Alway return 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >=  '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @t: THE integer yo be computed
+ * Return: return the absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
